294807129f3f770e368d82c8a5554487b8769ed0
Jonaxx8 <20r21a0543@mlrinstitutions.ac.in>
2023-10-02 10:02:38 +0530

    User model and register route created

---
+++ backend/routes/auth.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require('bcryptjs');
+
+//@route GET /api/auth/test
+//@desc Test the auth route
+//@access Public
+
+router.get("/test", (req, res) =>{
+    res.send("Auth is working");
+});
+
+//@route POST /api/auth/register
+//@desc Create a new user
+//@access Public
+router.post("/register", async (req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+        
+        //create a new user
+        const newUser = new User({
+            email: req.body.email,
+            password: hashedPassword,
+            name: req.body.name
+        });
+        
+        const savedUser = await newUser.save();
+
+        //return the new user
+        return res.json(savedUser);
+    }catch(e){
+        console.log(e);
+        res.send(500).send(e.message);
+    }
+})
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+//importing routes
+const authRoute = require("./routes/auth");
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.send("test");
 });
+
+app.use("/api/auth", authRoute);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to db");
